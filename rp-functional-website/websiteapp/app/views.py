8b3fc690b9bdbf687fd09eb5de051a2222113671
teambits009 <teambitsandy@gmail.com>
2023-10-13 20:36:41 +0300

    init.py

--- rp-functional-website/websiteapp/app/views.py
+++ rp-functional-website/websiteapp/app/views.py
@@ -51,6 +51,49 @@ def addNotice(request):
             if(form.is_valid()):
                 form.save()
                 return redirect('/')
+        context={'form':form}
+        return render(request,'app/addNotice.html', context)
+    else:
+        return redirect('home')
+    
+    def registerPage(request):
+        if request.user.is_authenticated:
+            return redirect('home')
+        else:
+            form= createuserform()
+            if request.method=='POST':
+                form= createuserform(request.POST)
+                if form.is_valid():
+                    user=form.save()
+                    return redirect('login')
+            context = {
+                'form': form,
+            }
+            return render (request,'app/register.html', context)
+        
+    def loginPage(request):
+        if request.user.is_authenticated:
+            return redirect('home')
+        else:
+            if request.user.is_authenticated:
+                return redirect('home')
+            else:
+                if request.method=='POST':
+                    username=request.POST.get('username')
+                    password=request.POST.get('password')
+                    user=authenticate(request,username=username,password=password)
+                    if user is not None:
+                        login(request,user)
+                        return redirect('/')
+                    
+                    context= { }
+                    return render (request,'app/login.html', context)
+                
+    def logoutPage(request):
+        logout(request)
+        return redirect('/')
+
+    
         
 
 
